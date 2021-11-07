7d9ae169f3792baa29c2c9ab86436236ac8a1986
omerShahar1 <93426230+omerShahar1@users.noreply.github.com>
2021-11-07 19:43:12 +0200

    Update NumClass.h

--- NumClass.h
+++ NumClass.h
@@ -4,7 +4,7 @@
 	int reversedNum(int);
 	int power(int, int);
 	int size1(int, int)
-	int reversedNum(int, int);
+	int reversedNum1(int, int);
 	int recursiveIsArmstrong(int, int, int);
 	int isArmstrong(int);
     	int isPalindrome(int);
