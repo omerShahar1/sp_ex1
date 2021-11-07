2fe5f4312d16723e600f9932ac25b334454ad68b
omerShahar1 <93426230+omerShahar1@users.noreply.github.com>
2021-11-07 19:47:32 +0200

    Update NumClass.h

--- NumClass.h
+++ NumClass.h
@@ -1,14 +1,10 @@
-#ifndef _NUMCLASS_H_
-	#define _NUMCLASS_H_
-    
-	int reversedNum(int);
-	int power(int, int);
-	int size1(int, int)
-	int reversedNum1(int, int);
-	int recursiveIsArmstrong(int, int, int);
-	int isArmstrong(int);
-    	int isPalindrome(int);
-    	int isPrime(int);
-    	int isStrong(int);
-   
-#endif
+int reversedNum(int num);
+int power(int base, int a);
+int size1(int num, int answer);
+int size(int num);
+int reversedNum1(int num, int answer);
+int recursiveIsArmstrong(int num, int digits, int sum);
+int isArmstrong(int);
+int isPalindrome(int);
+int isPrime(int);
+int isStrong(int);
