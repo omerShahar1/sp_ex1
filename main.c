e8ed0f903cdb891916fa1c6c5af7af77f17d9ae6
omerShahar1 <93426230+omerShahar1@users.noreply.github.com>
2021-11-07 02:03:28 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include "NumClass.h"
+
+int main() 
+{
+    int max, min, i, a, b;
+    scanf("%d %d", &a, &b);
+
+    if(a<b)
+    {
+        min = a;
+        max = b;
+    }
+    else
+    {
+        min = b;
+        max = a;
+    }
+
+    printf("/nThe Armstrong numbers are: ");
+    for(i=min; i<=max; i++)
+    {
+        if(isArmstrong(i) == 1)
+        {
+            printf("%d ",i);
+        }
+    }
+    printf("/nThe Palindromes numbers are: ");
+    for(i=min; i<=max; i++)
+    {
+        if(isPalindrome(i) == 1)
+        {
+            printf("%d ",i);
+        }
+    }
+    printf("/nThe Prime numbers are: ");
+    for(i=min; i<=max; i++)
+    {
+        if(isPrime(i) == 1)
+        {
+            printf("%d ",i);
+        }
+    }
+    printf("/nThe Strong numbers are: ");
+    for(i=min; i<=max; i++)
+    {
+        if(isStrong(i) == 1)
+        {
+            printf("%d ",i);
+        }
+    }
+    return 0;
+}
+
+
