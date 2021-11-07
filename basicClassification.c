33d6dade7f7c6c44ea25a1519eec9dd838b1a912
omerShahar1 <93426230+omerShahar1@users.noreply.github.com>
2021-11-07 20:03:58 +0200

    Update basicClassification.c

--- basicClassification.c
+++ basicClassification.c
@@ -4,7 +4,7 @@
 int isPrime(int num) //return 1 if number is prime. else return 0
 {
     int i;
-    for(i=2; i<num/2; i++)
+    for(i=2; i<num; i++)
     {
         if(num % i == 0)
         {
@@ -17,21 +17,22 @@ int isPrime(int num) //return 1 if number is prime. else return 0
 
 int isStrong(int num)// return 1 if the number is strong. else return 0
 {
-    int sum=0, digit, value=num, i, factorial;
-    while(value != 0)
+    int sum = 0;
+    int value = num;
+    while(value !=  0)
     {
-        digit = value % 10; //represent specific digit of the number
-        factorial = 1;
-        for(i=digit; i>0; i--)
+        int digit = value%10;
+        int digitValue = digit;
+        for(int i = 1; i < digitValue; i++)
         {
-            factorial = factorial * i; 
+            digit = digit * i;
         }
-        sum = sum + factorial;
+        sum = sum + digit;
         value = value/10;
     }
     if(sum == num)
     {
         return 1;
     }
-    return 0; 
+    return 0;
 }
